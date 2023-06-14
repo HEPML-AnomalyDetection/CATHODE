9b52c05538a0f10e41df8952d880795bc86fe097
Manuel Sommerhalder <m.r.sommerhalder@hotmail.com>
2023-06-14 08:55:02 +0200

    adding file download to data preparation bash script

--- run_data_preparation_latentCATHODE.sh
+++ run_data_preparation_latentCATHODE.sh
@@ -1,3 +1,6 @@
+wget https://zenodo.org/record/4536377/files/events_anomalydetection_v2.features.h5
+wget https://zenodo.org/record/5759087/files/events_anomalydetection_qcd_extra_inneronly_features.h5
+
 python run_data_preparation_latentCATHODE.py \
     --outdir latentCATHODE_data/no-shift_bkg-only \
     --S_over_B 0
